5ffdaa90e0bcc838ef4079b1238c3cddbb69f8ff
karthicbz <pkarthicbz@live.in>
2023-06-28 16:22:49 +0530

    Added files and changes required for initial deployment

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const catalogRouter = require("./routes/catalog");
 const passport = require("passport");
 const session = require("express-session");
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
 
 mongoose.set("strictQuery", false);
 
@@ -21,11 +28,11 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(limiter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(
   session({
@@ -48,6 +55,19 @@ const mongodb = process.env.MONGODB_URL;
   }
 })();
 
+//helmet
+app.use(
+  helmet.contentSecurityPolicy({
+    directives:{
+      "script-src":["'self", "cdnjs.cloudflare.com", "fonts.googleapis.com", "unpkg.com", "'unsafe-inline'"],
+    },
+  })
+);
+
+//compression
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/blog", catalogRouter);
 app.use("/users", usersRouter);
